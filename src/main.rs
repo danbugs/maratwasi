e1f52fa1c5a3747c8e06ffba81a83e2a6d8f273d
Dan Chiarlone <danilochiarlone@gmail.com>
2022-11-15 02:02:30 +0000

    providing root fs + mounted proc
    Signed-off-by: Dan Chiarlone <danilochiarlone@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
-use std::process::{exit, Command};
+use anyhow::bail;
+use nix::unistd::pivot_root;
+use std::{
+    os::unix::prelude::PermissionsExt,
+    path::Path,
+    process::{exit, Command},
+};
 
 fn main() {
     // get arguments
@@ -12,10 +18,59 @@ fn main() {
     }
 }
 
-fn parent() {
-    // parent says hello
-    println!("Hello from parent");
+fn provide_root_filesystem() -> anyhow::Result<()> {
+    let new_root = "./rootfs";
+    let rootfs = Path::new(new_root).join(".pivot_root/");
 
+    // bind mount newroot to itself
+    nix::mount::mount(
+        Some(new_root),
+        new_root,
+        None::<&str>,
+        nix::mount::MsFlags::MS_BIND,
+        None::<&str>,
+    )?;
+
+    // create rootfs directory with 0700 permissions
+    std::fs::create_dir_all(&rootfs)?;
+    std::fs::set_permissions(&rootfs, std::fs::Permissions::from_mode(0o700))?;
+
+    // call pivot_root
+    pivot_root(new_root, &rootfs)?;
+
+    // ensure current working directory is set to new
+    std::env::set_current_dir("/")?;
+
+    // unmount old rootfs
+    nix::mount::umount2("/.pivot_root", nix::mount::MntFlags::MNT_DETACH)?;
+
+    // remove old rootfs
+    std::fs::remove_dir_all("/.pivot_root")?;
+
+    Ok(())
+}
+
+fn mount_proc() -> anyhow::Result<()> {
+    let new_root = "./rootfs";
+    let target = Path::new(new_root).join("proc/");
+
+    // make target dir and set permissions to 0755
+    std::fs::create_dir_all(&target)?;
+    std::fs::set_permissions(&target, std::fs::Permissions::from_mode(0o755))?;
+
+    // mount proc
+    nix::mount::mount(
+        Some("proc"),
+        &target,
+        Some("proc"),
+        nix::mount::MsFlags::empty(),
+        None::<&str>,
+    )?;
+
+    Ok(())
+}
+
+fn parent() {
     // vector with namespaces to unshare (i.e., mount, uts, ipc, pid, net, and user)
     let namespaces = vec![
         unshare::Namespace::Mount,
@@ -62,10 +117,12 @@ fn parent() {
 }
 
 fn child() {
-    // child says hello
-    println!("Hello from child");
-
     let mut executor = unshare::Command::new("/bin/sh");
+
+    mount_proc().unwrap_or_else(|e| {
+        eprintln!("Failed to mount proc: {}", e);
+        exit(1);
+    });
 
     // get pid
     let pid = std::process::id();
@@ -78,6 +135,11 @@ fn child() {
             eprintln!("Failed to run process: {}", e);
             exit(1);
         });
+
+    provide_root_filesystem().unwrap_or_else(|e| {
+        eprintln!("Failed to provide root filesystem: {}", e);
+        exit(1);
+    });
 
     let mut child = executor.spawn().unwrap_or_else(|e| {
         eprintln!("Failed to spawn process: {}", e);
